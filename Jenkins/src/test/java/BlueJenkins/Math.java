3adfaeedad0dd21881c6825f04f5821de5251570
Esmasen312 <esmasen312@gmail.com>
2020-08-05 14:42:26 -0500

    old commit

---
+++ Jenkins/src/test/java/BlueJenkins/Math.java
@@ -0,0 +1,38 @@
+package BlueJenkins;
+
+import org.testng.annotations.Test;
+
+public class Math {
+	
+	 @Test
+	 public void unit()
+     {
+    	 System.out.println("istatistic");
+    	 
+     }
+	 
+	 @Test
+	 public void number()
+	 {
+		 System.out.println("biggest");
+	 }
+	 
+	 @Test
+	 public void teacher()
+	 {
+		 System.out.println("Chinese");
+	 }
+
+	 @Test
+	 public void student()
+	 {
+		 System.out.println("immigrant");
+	 }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            
+	}
+
+}
